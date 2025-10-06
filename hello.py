50ecc7e69804a6127f0464ccb6ccc2cad382f584
AshaS591 <as1579119@gmail.com>
2025-10-06 10:38:39 +0530

    v4.1

--- hello.py
+++
@@ -1,2 +0,0 @@
-print("Hello from SWUpdate!")
-print("This is a sample Python script.")
--- rootfs/usr/local/bin/hello.py
+++ rootfs/usr/local/bin/hello.py
@@ -1,2 +1,3 @@
 print("Hello from SWUpdate!")
 print("This is asha...")
+print("This is a sample Python script.")
